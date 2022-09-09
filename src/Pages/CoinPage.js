ff34ba091a138ea75df33ef961ea777cd8e1f984
kartavya <kartavyas@gmail.com>
2022-09-09 17:25:33 +1000

    working on singal coin page

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { SingleCoin } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core";
 
 const CoinPage = () => {
-  return <div>Coin page</div>;
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+
+  console.log(coin);
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const useStyles = makeStyles(() => ({}));
+
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <div className={classes.sidebar}>{/* {sidebar} */}</div>
+      {}
+    </div>
+  );
 };
 
 export default CoinPage;
